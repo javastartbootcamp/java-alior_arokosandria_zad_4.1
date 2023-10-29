bdb4dbb82eabe089ac5ab5f5d1162b9f6f9b0a3c
arokosandria <kmsa@interia.pl>
2023-10-29 18:39:48 +0100

    solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,11 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        Room room = new Room(120, 30, true, 100);
+        Room room1 = new Room(100.09, 30, true, 100);
+        System.out.println(room.lowerTemperature());
+        System.out.println(room.getTemperature());
+        System.out.println(room1.lowerTemperature());
+        System.out.println(room1.getTemperature());
     }
 }
